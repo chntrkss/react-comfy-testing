1c16478e3c1d55ce3596b890e6990a071a7e266a
Cihan <cihanturkess@gmail.com>
2022-03-30 22:00:24 +0300

    mini fixed

--- functions/create-payment-intent.js
+++ functions/create-payment-intent.js
@@ -5,7 +5,7 @@ const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET_KEY)
 
 exports.handler = async function (event, context) {
     if (event.httpMethod === "POST") {
-        const { cart, shipping_fee, total_amount } = JSON.parse(event.body)
+        const { shipping_fee, total_amount } = JSON.parse(event.body)
 
         const calculateOrderAmount = () => shipping_fee + total_amount
 
