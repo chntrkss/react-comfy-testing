5270d3b216f288524d29dffc1414fc261143e420
Cihan <cihanturkess@gmail.com>
2022-03-30 21:07:29 +0300

    first commit

---
+++ src/pages/PrivateRoute.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Navigate } from "react-router-dom"
+import { useAuth0 } from "@auth0/auth0-react"
+
+const PrivateRoute = ({ children }) => {
+    const { user } = useAuth0()
+    if (!user) {
+        return <Navigate to={"/"} />
+    }
+    return children
+}
+export default PrivateRoute
+
+// import React from "react"
+// import { Route, Redirect } from "react-router-dom"
+// import { useAuth0 } from "@auth0/auth0-react"
+
+// const PrivateRoute = ({ children, ...rest }) => {
+//     const { user } = useAuth0()
+
+//     return (
+//         <Route {...rest} render={() => {
+//             return user ? children : <Redirect to={"/"} />
+//         }}></Route>
+//     )
+// }
+// export default PrivateRoute
